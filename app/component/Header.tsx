fc94aace5ac1152dd0adc914ec87e0a6fc6c69a7
Naveen-shaikh <shaikhazan0@gmail.com>
2024-11-10 00:17:36 +0500

    assignment08

---
+++ app/component/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header() {
+  return <div className="h-[88px] bg-amber-600	 font-italic  font-extrabold text-lg	">
+    PURITY 
+  </div>;
+}
+
+export default Header;
